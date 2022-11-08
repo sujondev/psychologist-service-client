aeb094affe86103416a3e945b32a88c579d194da
sujondev <emd26521@gmail.com>
2022-11-08 12:17:29 +0600

    header adding

---
+++ src/Pages/Shared/Header/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './Header.css'
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home"><img src="http://azim.commonsupport.com/Morriston/images/logo.png" alt="" /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Link className='text-decoration-none nav-item me-4' to="/">Home</Link>
+                            <Link className='text-decoration-none nav-item me-4' to="/blog">Blog</Link>
+                            <Link className='text-decoration-none nav-item me-4' to="/login">Login</Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
